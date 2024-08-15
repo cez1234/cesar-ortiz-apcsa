c90df8433e5ac80329ce3ba1e12818b7c47a5dc5
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-08-15 21:04:50 +0000

    finished first daily code

--- dailycoding/TigersRock.java
+++ dailycoding/TigersRock.java
@@ -1,1 +1,10 @@
+class TigersRock {
+    
+    public static void main (String[] args) {
+    System.out.println("Lincoln\n\n");
+    System.out.println("Tigers\n\n");
+    System.out.print("Rock!");
+    }
+
+}
 
