e4a3f5c4edccfee949da2642897baadf34205da6
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-12 19:51:21 +0000

    fixed multiples

--- dailycoding/Multiples.java
+++ dailycoding/Multiples.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 class Multiples {
-    public static void main {
+    public static void main (String[] args) {
         Scanner scan = new Scanner(System.in );
         int num;
         System.out.println("Input an integer between 1-10");
+        
     }
 }
