c67255c5a7de154be22d2c7ac3068345b4d053f6
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-12 15:45:15 +0000

    poem, multiples, trapezoid area

---
+++ dailycoding/FriendsPoem.java
@@ -0,0 +1,8 @@
+class FriendsPoem {
+public static void main (String[] args) {
+       
+    System.out.print("Roses are red, \n\t Violets are blue,\nSugar is sweet\n\tBut I have \"commitment issues\"\n\tSo I'd rather just be friends\n\tAt this point in our relationship");
+
+
+    }
+}
