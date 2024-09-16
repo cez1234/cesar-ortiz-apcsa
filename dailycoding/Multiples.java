f28d7383b70dc7e227c68a70ac2ac87c6d9c1408
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-16 19:43:28 +0000

    completed "multiples" daily coding

--- dailycoding/Multiples.java
+++ dailycoding/Multiples.java
@@ -5,6 +5,7 @@ class Multiples {
         Scanner scan = new Scanner(System.in );
         int num;
         System.out.println("Input an integer between 1-10");
-        
+        num = scan.nextInt();
+        System.out.println("The multiples of " + num + " are: " + num + " " + (num*num) + " " + (num*num*num) + " " + (num*num*num*num) + " " + (num*num*num*num*num));
     }
 }
