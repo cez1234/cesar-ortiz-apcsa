63a1eb201793d4be523a4a47bcffe243155ea4ca
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-16 19:55:38 +0000

    completed "DaysToMinutes" daily coding

--- dailycoding/DaysToMinutes.java
+++ dailycoding/DaysToMinutes.java
@@ -4,6 +4,12 @@ class DaysToMinutes {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        
+        int days;
+        int mins;
+
+        System.out.println("Enter a number of days: ");
+        days = scan.nextInt();
+        mins = days * 24 * 60;
+        System.out.println("There are " + mins + " minutes in " + days + " days.");
     }
 }
