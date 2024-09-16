3de3e184fb9068b26e4592538bc09d03ee97894d
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-16 19:50:14 +0000

    completed "BeforeAndAfter" daily coding

--- dailycoding/BeforeAndAfter.java
+++ dailycoding/BeforeAndAfter.java
@@ -4,6 +4,9 @@ class BeforeAndAfter {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        
-    }
+        int num;
+
+        System.out.println("Please enter an integer: ");
+        num = scan.nextInt();
+        System.out.println("The three numbers before " + num + " are: " + (num-1) + " " + (num-2) + " " + (num-3) + ". The three numbers after " + num + " are: " + (num+1) + " " + (num+2) + " " + (num+3) + ".");    }
 }
