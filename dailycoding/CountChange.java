35f73f523f95fc1c52957e5f041f6a289f5f2f71
Cesar O. <143016551+cez1234@users.noreply.github.com>
2024-09-18 19:50:37 +0000

    completed "CountChange" daily coding

--- dailycoding/CountChange.java
+++ dailycoding/CountChange.java
@@ -4,6 +4,22 @@ class CountChange {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int penny;
+        int nickel;
+        int dime;
+        int quarter;
+        double total;
+
+        System.out.print("Enter a number of pennies: ");
+        penny = scan.nextInt();
+        System.out.print("Enter a number of nickels: ");
+        nickel = scan.nextInt();
+        System.out.print("Enter a number of dimes: ");
+        dime = scan.nextInt();
+        System.out.print("Enter a number of quarters: ");
+        quarter = scan.nextInt();
+        total = (0.01 * penny) + (0.05 * nickel) + (0.1 * dime) + (0.25 * quarter);
+        System.out.printf("%.2f", total);
         
-    }
+        }
 }
